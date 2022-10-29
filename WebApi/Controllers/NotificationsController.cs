82e9c6fba35fbcc1ee6ebe2d268724e39210add7
Kamil Żuk <kamilzuk24@o2.pl>
2022-10-29 13:32:18 +0200

    add hubs

---
+++ WebApi/Controllers/NotificationsController.cs
@@ -0,0 +1,86 @@
+﻿namespace WebApi.Controllers
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Net;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using WebApi.Models;
+    using WebApi.Services;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationsController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        [HttpGet]
+        [Route("test")]
+        public async Task<IActionResult> Test()
+        {
+            return new OkResult();
+        }
+
+        [HttpPut]
+        [Route("installations")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.UnprocessableEntity)]
+        public async Task<IActionResult> UpdateInstallation(
+    [Required] DeviceInstallation deviceInstallation)
+        {
+            var success = await _notificationService
+                .CreateOrUpdateInstallationAsync(deviceInstallation, HttpContext.RequestAborted);
+
+            if (!success)
+                return new UnprocessableEntityResult();
+
+            return new OkResult();
+        }
+
+        [HttpDelete()]
+        [Route("installations/{installationId}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.UnprocessableEntity)]
+        public async Task<ActionResult> DeleteInstallation(
+            [Required][FromRoute] string installationId)
+        {
+            var success = await _notificationService
+                .DeleteInstallationByIdAsync(installationId, CancellationToken.None);
+
+            if (!success)
+                return new UnprocessableEntityResult();
+
+            return new OkResult();
+        }
+
+        [HttpPost]
+        [Route("requests")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.UnprocessableEntity)]
+        public async Task<IActionResult> RequestPush(
+            [Required] NotificationRequest notificationRequest)
+        {
+            if ((notificationRequest.Silent &&
+                string.IsNullOrWhiteSpace(notificationRequest?.Action)) ||
+                (!notificationRequest.Silent &&
+                string.IsNullOrWhiteSpace(notificationRequest?.Text)))
+                return new BadRequestResult();
+
+            var success = await _notificationService
+                .RequestNotificationAsync(notificationRequest, HttpContext.RequestAborted);
+
+            if (!success)
+                return new UnprocessableEntityResult();
+
+            return new OkResult();
+        }
+    }
+}
---
+++ WebApi/Services/INotificationService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Services
+{
+    public interface INotificationService
+    {
+        Task<bool> CreateOrUpdateInstallationAsync(DeviceInstallation deviceInstallation, CancellationToken token);
+
+        Task<bool> DeleteInstallationByIdAsync(string installationId, CancellationToken token);
+
+        Task<bool> RequestNotificationAsync(NotificationRequest notificationRequest, CancellationToken token);
+    }
+}
